581efb421dbcd279af622621758eaf23b494c191
RoshanMhjn <roshanmaharjan948@gmail.com>
2025-07-28 11:51:31 +0545

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+
+const App = () => {
+  return (
+    <div className="text-7xl">Home
+    
+    <Button variant="outline">Click</Button>
+    </div>
+  )
+}
+
+export default App
